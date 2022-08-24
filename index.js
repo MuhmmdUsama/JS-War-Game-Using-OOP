54b3a67e1db3aa3d1e1765c6885363c51b117065
Muhmmd-Usama <mummdusama@gmail.com>
2022-08-24 19:11:00 +0200

    Create constractor function for character

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function Character (name,strength,health){
+    this.strength = strength,
+    this.health = health,
+    this.name = name
+}
+
+let nartoo = new Character('Nartoo',10,100)
+let sasuka = new Character('Sasuka',10,100)
+
+console.log(nartoo)
